73503740d03c140e0eef30bdc9e734f3a3a999df
MaroM <maro.muszynski@gmail.com>
2019-04-15 19:35:02 +0200

    add class named JwtProvider. It gets username from Authentication object, then builds JWT Token with username, Date() object, secretKey. JwtProvider can also be used to validate JWT Token

---
+++ jwtauthentication/src/main/java/com/marom/jwtauthentication/security/jwt/JwtProvider.java
@@ -0,0 +1,60 @@
+package com.marom.jwtauthentication.security.jwt;
+
+import com.marom.jwtauthentication.security.service.UserPrinciple;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+
+import java.util.Date;
+
+@Slf4j
+public class JwtProvider {
+
+
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${app.jwtExpiration}")
+    private int jwtExpiration;
+
+    public String generateJwtToken(Authentication authentication) {
+
+        UserPrinciple userPrincipal = (UserPrinciple) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpiration*1000))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            log.error("Invalid JWT signature -> Message: {} ", e);
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token -> Message: {}", e);
+        } catch (ExpiredJwtException e) {
+            log.error("Expired JWT token -> Message: {}", e);
+        } catch (UnsupportedJwtException e) {
+            log.error("Unsupported JWT token -> Message: {}", e);
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty -> Message: {}", e);
+        }
+
+        return false;
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody().getSubject();
+    }
+}
