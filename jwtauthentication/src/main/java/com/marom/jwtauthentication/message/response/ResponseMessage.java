84c8a0f81a8170b72a4bb3c9a18c4b1e5e69ff1c
MaroM <maro.muszynski@gmail.com>
2019-04-16 08:51:51 +0200

    add missing lombok constructor annotation

--- jwtauthentication/src/main/java/com/marom/jwtauthentication/message/response/ResponseMessage.java
+++ jwtauthentication/src/main/java/com/marom/jwtauthentication/message/response/ResponseMessage.java
@@ -1,8 +1,12 @@
 package com.marom.jwtauthentication.message.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseMessage {
 
     private String message;
